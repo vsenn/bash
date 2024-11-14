ff9ec1d55c5d2738499c83e2013d9f009af4be47
Valery Semichev <valery.semichev@gmail.com>
2024-11-14 13:40:22 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Various bash scripts
---
+++ isDesktopOnly.sh
@@ -0,0 +1,70 @@
+$ cd ~/Documents/Obsidian/.obsidian/plugins
+$ cat ai-image-analyzer/manifest.json | grep isDesktopOnly
+	"isDesktopOnly": true
+$ cat beautitab/manifest.json | grep isDesktopOnly
+	"isDesktopOnly": false
+$ cat calendar/manifest.json | grep isDesktopOnly
+  "isDesktopOnly": false,
+$ cat dataview/manifest.json | grep isDesktopOnly
+  "isDesktopOnly": false
+$ cat extract-url/manifest.json | grep isDesktopOnly
+  "isDesktopOnly": true,
+$ cat file-explorer-plus/manifest.json | grep isDesktopOnly
+  "isDesktopOnly": true
+$ cat hide-folders/manifest.json | grep isDesktopOnly
+  "isDesktopOnly": false
+$ cat highlightr-plugin/manifest.json | grep isDesktopOnly
+  "isDesktopOnly": true
+$ cat ibook/manifest.json | grep isDesktopOnly
+  "isDesktopOnly": true
+$ cat image2latex/manifest.json | grep isDesktopOnly
+cat: image2latex/manifest.json: No such file or directory
+$ cat multi-properties/manifest.json | grep isDesktopOnly
+  "isDesktopOnly": true
+$ cat multi-tag/manifest.json | grep isDesktopOnly
+  "isDesktopOnly": true
+$ cat obsidian-calibre-plugin/manifest.json | grep isDesktopOnly
+  "isDesktopOnly": true
+$ cat obsidian-excalidraw-plugin/manifest.json | grep isDesktopOnly
+  "isDesktopOnly": true
+$ cat obsidian-focus-plugin/manifest.json | grep isDesktopOnly
+	"isDesktopOnly": false
+$ cat obsidian-icon-folder/manifest.json | grep isDesktopOnly
+  "isDesktopOnly": true
+$ cat obsidian-importer/manifest.json | grep isDesktopOnly
+	"isDesktopOnly": false
+$ cat obsidian-link-embed/manifest.json | grep isDesktopOnly
+  "isDesktopOnly": false
+$ cat obsidian-linter/manifest.json | grep isDesktopOnly
+  "isDesktopOnly": true
+$ cat obsidian-livesync/manifest.json | grep isDesktopOnly
+    "isDesktopOnly": false
+$ cat obsidian-mindmap-nextgen/manifest.json | grep isDesktopOnly
+  "isDesktopOnly": true,
+$ cat obsidian-tasks-plugin/manifest.json | grep isDesktopOnly
+  "isDesktopOnly": true
+$ cat omnisearch/manifest.json | grep isDesktopOnly
+  "isDesktopOnly": false
+$ cat table-editor-obsidian/manifest.json | grep isDesktopOnly
+  "isDesktopOnly": true,
+$ cat quick-links/manifest.json | grep isDesktopOnly
+  "isDesktopOnly": true
+$ cat quick-tagger/manifest.json | grep isDesktopOnly
+  "isDesktopOnly": true
+$ cat sheet-plus/manifest.json | grep isDesktopOnly
+cat: sheet-plus/manifest.json: No such file or directory
+$ cat shiki-highlighter/manifest.json | grep isDesktopOnly
+	"isDesktopOnly": false
+$ cat smart-connections/manifest.json | grep isDesktopOnly
+cat: smart-connections/manifest.json: No such file or directory
+$ cat smart-second-brain/manifest.json | grep isDesktopOnly
+    "isDesktopOnly": true
+$ cat surfing/manifest.json | grep isDesktopOnly
+cat: surfing/manifest.json: No such file or directory
+$ cat text-extractor/manifest.json | grep isDesktopOnly
+  "isDesktopOnly": true
+$ cat widgets/manifest.json | grep isDesktopOnly
+	"isDesktopOnly": false
+$ cat wikipedia-search/manifest.json | grep isDesktopOnly
+  "isDesktopOnly": true
+$
